75d4a7068a481b133ab610a33f7849036883c56f
Gustavo Garcia <gustavosgarcia2005@gmail.com>
2024-11-12 11:00:05 -0300

    adição carro3

--- main.py
+++ main.py
@@ -14,9 +14,17 @@ fundo = pygame.image.load("assets/fundo.png")
 
 carro1 = pygame.image.load("assets/carro1.png")
 carro2 = pygame.image.load("assets/carro2.png")
+carro3 = pygame.image.load("assets/carro3.png")
 
-movXCar1, movXCar2, = 25, 25
-posYCar1, posYCar2, = 50, 180
+movXCar1, movXCar2, movXCar3 = 25, 25, 25
+posYCar1, posYCar2, posYCar3 = 50, 180, 110
+
+vitoria = pygame.mixer.Sound("assets/vitoria.mp3")
+vitoria.set_volume(0.5)
+pygame.mixer.music.load("assets/trilha.mp3")
+pygame.mixer.music.play(-1) #-1 looping, 1,2 3 vezes
+acabou = False
+somDaVitoria = False
 
 while True:
     for evento in pygame.event.get(): 
@@ -27,6 +35,30 @@ while True:
     tela.blit(fundo, (0,0))
     tela.blit(carro1, (movXCar1,posYCar1))
     tela.blit(carro2, (movXCar2,posYCar2))
+    tela.blit(carro3, (movXCar3,posYCar3))
+    
+    if not acabou :
+        movXCar1 = movXCar1 + random.randint(0,10)
+        movXCar2 = movXCar2 + random.randint(0,10)
+        movXCar3 = movXCar3 + random.randint(0,10)
+    else:
+        pygame.mixer.music.stop()
+        if somDaVitoria == False:
+            pygame.mixer.Sound.play(vitoria)
+            somDaVitoria = True
+        
+    
+    if movXCar1 > 1000:
+        movXCar1 = 0
+        posYCar1 = 350
+        
+    if movXCar2 > 1000:
+        movXCar2 = 0
+        posYCar2 = 480
+        
+    if movXCar3 > 1000:
+        movXCar3 = 0
+        posYCar3 = 400
     
     pygame.display.update()
     clock.tick(60)
