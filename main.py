25638da629b099a68d5d5c2d0768bc135b24af50
Gustavo Garcia <gustavosgarcia2005@gmail.com>
2024-11-12 16:17:32 -0300

    atualização read me

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ pygame.init()
 tamanho = (1000,592)
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode( tamanho )
-icone = pygame.image.load("assets/icone.ico")
+icone = pygame.image.load("Recursos/icone.ico")
 pygame.display.set_icon(icone)
 pygame.display.set_caption('Corrida Maluca') #texto em cima 
 branco = (255,255,255)
 preto = (0,0,0)
-fundo = pygame.image.load('assets/fundo.png')
-carro1 = pygame.image.load('assets/carro1.png')
-carro2 = pygame.image.load('assets/carro2.png')
-carro3 = pygame.image.load('assets/carro3.png')
+fundo = pygame.image.load('Recursos/fundo.png')
+carro1 = pygame.image.load('Recursos/carro1.png')
+carro2 = pygame.image.load('Recursos/carro2.png')
+carro3 = pygame.image.load('Recursos/carro3.png')
 
 movXCar1, movXCar2, movXCar3 = 0, 0, 0
 posYCar1, posYCar2, posYCar3 = 25, 115, 205
 
-vitoria = pygame.mixer.Sound('assets/vitoria.mp3')
+vitoria = pygame.mixer.Sound('Recursos/vitoria.mp3')
 vitoria.set_volume(0.5)
-pygame.mixer.music.load('assets/trilha.mp3')
+pygame.mixer.music.load('Recursos/trilha.mp3')
 pygame.mixer.music.play(-1)#looping
 acabou, somDaVitoria = False, False
 
@@ -86,9 +86,44 @@ while True:
     movXCar1, movXCar2, movXCar3, posYCar1, posYCar2, posYCar3 = carros_segunda_pista(movXCar1, movXCar2, movXCar3, posYCar1, posYCar2, posYCar3)
         
     fonte = pygame.font.Font('freesansbold.ttf',60)#ttf é o arquivo da font
-    textoVermelho = fonte.render('Vermelho Ganhou!', True, branco)
-    textoAmarelo = fonte.render('Amarelo Ganhou!', True, branco)
-    textoAzul = fonte.render('Azul Ganhou!', True, branco)
+    textoVermelho = fonte.render('Vermelho Ganhou!', True, preto)
+    textoAmarelo = fonte.render('Amarelo Ganhou!', True, preto)
+    textoAzul = fonte.render('Azul Ganhou!', True, preto)
+    
+    if posYCar1 == 330 and movXCar1 >= 900 and (movXCar1 > movXCar2 and movXCar1 > movXCar3):
+        tela.blit(textoVermelho, (270,70))
+        acabou = True
+        
+    elif posYCar2 == 410 and movXCar2 >= 900 and (movXCar2 > movXCar1 and movXCar2 > movXCar3):
+        tela.blit(textoAmarelo, (270,180))
+        acabou = True
+
+    elif posYCar3 == 490 and movXCar3 >= 900 and (movXCar3 > movXCar1 and movXCar3 > movXCar2):
+        tela.blit(textoAzul, (270,180))
+        acabou = True
+        
+    if acabou:
+        tela.fill(preto)
+        novo_fundo = pygame.image.load('Recursos/finalbackground.jpg')
+        tela.blit(novo_fundo, (0, 0))
+
+        fonte_final = pygame.font.SysFont('arial', 30)
+        distancia_primeiro_segundo = abs(movXCar1 - movXCar2) if firstPlace == 'Vermelho' and secondPlace == 'Amarelo' or firstPlace == 'Amarelo' and secondPlace == 'Vermelho' else abs(movXCar1 - movXCar3) if firstPlace == 'Vermelho' else abs(movXCar2 - movXCar3)
+        distancia_segundo_terceiro = abs(movXCar2 - movXCar3) if thirdPlace == 'Azul' else abs(movXCar1 - movXCar2)
+
+        # Textos para cada colocação com distâncias
+        texto_vencedor_final = fonte_final.render(f'1º lugar: Carro {firstPlace}', True, preto)
+        texto_distancia_1_2 = fonte_final.render(f'Distância para o 2º lugar: {distancia_primeiro_segundo} pixels', True, preto)
+        texto_segundo_final = fonte_final.render(f'2º lugar: Carro {secondPlace}', True, preto)
+        texto_distancia_2_3 = fonte_final.render(f'Distância para o 3º lugar: {distancia_segundo_terceiro} pixels', True, preto)
+        texto_terceiro_final = fonte_final.render(f'3º lugar: Carro {thirdPlace}', True, preto)
+
+        tela.blit(texto_vencedor_final, (100, 100)) 
+        tela.blit(texto_distancia_1_2, (100, 160))  
+        tela.blit(texto_segundo_final, (100, 220))  
+        tela.blit(texto_distancia_2_3, (100, 280))  
+        tela.blit(texto_terceiro_final, (100, 340)) 
+        
         
 
     pygame.display.update()
