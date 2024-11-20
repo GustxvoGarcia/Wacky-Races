187fe84738b10aaddc8c4be49a38b6cf380735b8
Marcos Ferreira <marcosmeribe@gami.com>
2024-11-20 19:47:20 -0300

    adição setup

--- geraSetup.py
+++ geraSetup.py
@@ -1,15 +1,18 @@
 import cx_Freeze
 
 executables = [
-    cx_Freeze.Executable(script= 'main.py', icon='assets/icone.ico')
+    cx_Freeze.Executable(script="main.py", icon="Recursos/icone.ico")
 ]
+
 cx_Freeze.setup(
-    name = 'Corrida Maluca',
-    options= {
-        'build_exe':{
-            'packages':['pygame'],
-            'include_files':['assets']
+    name="WACKY-RACES",
+    options={
+        "build_exe": {
+            "packages": ["pygame"],
+            "include_files": ["Recursos", "ReadMe.md"],
         }
-    }, executables = executables    
+    },
+    executables=executables,
 )
-#python geraSetup.py build || python geraSetup.py bdist_msi
+
+ 
